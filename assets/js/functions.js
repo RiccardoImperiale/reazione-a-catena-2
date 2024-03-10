226755c5a2524ac2c050704b6b0d84126c2e0b82
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-10 21:14:02 +0100

    export missing functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -28,16 +28,107 @@ export function renderNumberOfItems(numOfItems) {
     cartItems.innerHTML = `ITEMS - ${numOfItems}`;
 };
 
+// add each total products prices
+export function calcCartTotal() {
+    let cartTotal = 0;
+    const totalPrices = document.querySelectorAll(".total_price");
+    let totalText = '';
+    totalPrices.forEach(totalPrice => {
+        totalText = totalPrice.innerText.replace('€', '');
+        cartTotal += Number(totalText);
+    });
+    return cartTotal;
+};
+
 // render cart total to order summary
-export function renderCartTotal(cartTotal) {
+export function renderCartTotal() {
+    let cartTotal = calcCartTotal()
     const totalRender = document.querySelector("#total");
     totalRender.innerHTML = cartTotal.toFixed(2) + ' €';
 };
 
-export function addDeliveryPrice(shippingChoice, cartFullPrice,cartTotal) {
+export function increaseQuantity(button, index) {
+    button.addEventListener('click', () => {
+        let cartTotal = calcCartTotal();
+        const quantityInput = document.getElementById(`quantity${index}`);
+        const totalPrice = document.getElementById(`total_price${index}`);
+        const price = quantityInput.dataset.price;
+        quantityInput.value = Number(quantityInput.value) + 1;
+        totalPrice.innerText = Number(price * quantityInput.value).toFixed(2) + " €";
+        cartTotal += Number(price);
+        renderCartTotal();
+    });
+};
+
+export function decreaseQuantity(button, index) {
+    button.addEventListener('click', () => {
+        let cartTotal = calcCartTotal()
+        const quantityInput = document.getElementById(`quantity${index}`);
+        const totalPrice = document.getElementById(`total_price${index}`);
+        const price = quantityInput.dataset.price;
+        const finalPrice = totalPrice.innerText.replace('€', '');
+
+        if (Number(quantityInput.value) > 1) {
+            quantityInput.value = Number(quantityInput.value) - 1;
+            totalPrice.innerText = Number(finalPrice - price).toFixed(2) + " €";
+            cartTotal -= Number(price);
+            renderCartTotal();
+        }
+    });
+};
+
+export function removeProduct(btn, index, tableEl, numOfItems) {
+    btn.addEventListener('click', () => {
+        let cartTotal = calcCartTotal()
+        let childToRemove = tableEl.querySelector(`.single_product${index}`);
+        const totalPrice = document.getElementById(`total_price${index}`);
+        const priceToSubtract = totalPrice.innerHTML.replace('€', '');
+        cartTotal -= Number(priceToSubtract);
+        tableEl.removeChild(childToRemove);
+        numOfItems--;
+        renderCartTotal(cartTotal);
+        renderNumberOfItems(numOfItems);
+    });
+};
+
+export function addDeliveryPrice(shippingChoice, cartFullPrice, cartTotal) {
     cartFullPrice = cartTotal;
     shippingChoice == 5 && (cartFullPrice += 5);
     shippingChoice == 7 && (cartFullPrice += 7);
     shippingChoice == 12 && (cartFullPrice += 12);
     return cartFullPrice;
+};
+
+export function addPromoCode(promoCode, cartFullPrice, promoCodes) {
+    const promoMessage = document.getElementById('promoCodeMsg');
+    promoMessage.innerText = '';
+
+    if (promoCode == '') {
+        promoMessage.innerText = '';
+    } else if (!promoCodes.includes(promoCode)) {
+        promoMessage.innerText = 'Invalid Promo Code :(';
+        promoMessage.style.color = '#fa7272';
+    } else {
+        promoCode == 'megaultramaxipromo50' && (cartFullPrice -= cartFullPrice / 100 * 50);
+        promoCode == 'kebaboverdose80' && (cartFullPrice -= cartFullPrice / 100 * 80);
+        promoCode == 'fatgang25' && (cartFullPrice -= cartFullPrice / 100 * 25);
+        promoCode == 'eat-eat-repeat10' && (cartFullPrice -= cartFullPrice / 100 * 10);
+        promoMessage.innerText = `${promoCode.slice(-2)}% Promo Applied :)`
+        promoMessage.style.color = '#58c46a';
+    };
+
+    return cartFullPrice;
+};
+
+export function submitForm(cartFullPrice, promoCodes, totalCost) {
+    let cartTotal = calcCartTotal();
+
+    document.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const shippingChoice = document.querySelector('select').value;
+        const promoCode = document.querySelector('#promo_code').value;
+        cartFullPrice = addDeliveryPrice(shippingChoice, cartFullPrice, cartTotal);
+        cartFullPrice = addPromoCode(promoCode, cartFullPrice, promoCodes);
+        totalCost.innerHTML = cartFullPrice.toFixed(2) + ' €'; // render the price including delivery and discount if applied 
+    });
 };
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-import { render, renderNumberOfItems, renderCartTotal, addDeliveryPrice } from './functions.js';
+import { render, renderNumberOfItems, calcCartTotal, renderCartTotal, increaseQuantity, decreaseQuantity, removeProduct, submitForm } from './functions.js';
 
 const products = [
     {
@@ -30,13 +30,11 @@ const products = [
 const promoCodes = ['megaultramaxipromo50', 'kebaboverdose80', 'eat-eat-repeat10', 'fatgang25'];
 
 let numOfItems = products.length;
-let cartTotal = 0;
-let cartFullPrice = cartTotal;
+let cartFullPrice = calcCartTotal();
 
 const tableEl = document.querySelector("table > tbody");
 
-// add products to cart
-products.forEach((product, index) => render(product, index, tableEl));
+products.forEach((product, index) => render(product, index, tableEl)); // add products to cart
 
 const addButtons = document.querySelectorAll(".add-button");
 const removeButtons = document.querySelectorAll(".remove-button");
@@ -45,91 +43,10 @@ const removeProductBtns = document.querySelectorAll('.remove_product');
 
 renderNumberOfItems(numOfItems); // render number of products added to cart
 calcCartTotal(); // calculate cart total
-renderCartTotal(cartTotal); // render cart total
+renderCartTotal(); // render cart total
 
 addButtons.forEach((button, index) => increaseQuantity(button, index));
 removeButtons.forEach((button, index) => decreaseQuantity(button, index));
-removeProductBtns.forEach((btn, index) => removeProduct(btn, index));
+removeProductBtns.forEach((btn, index) => removeProduct(btn, index, tableEl, numOfItems)); // delete product
 
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const shippingChoice = document.querySelector('select').value;
-    const promoCode = document.querySelector('#promo_code').value;
-    cartFullPrice = addDeliveryPrice(shippingChoice, cartFullPrice, cartTotal);
-    applyPromoCode(promoCode);
-    totalCost.innerHTML = cartFullPrice.toFixed(2) + ' €'; // render the price including delivery and discount if applied 
-});
-
-function increaseQuantity(button, index) {
-    button.addEventListener('click', () => {
-        const quantityInput = document.getElementById(`quantity${index}`);
-        const totalPrice = document.getElementById(`total_price${index}`);
-        const price = quantityInput.dataset.price;
-        quantityInput.value = Number(quantityInput.value) + 1;
-        totalPrice.innerText = Number(price * quantityInput.value).toFixed(2) + " €";
-        cartTotal += Number(price);
-        renderCartTotal(cartTotal);
-    });
-};
-
-function decreaseQuantity(button, index) {
-    button.addEventListener('click', () => {
-        const quantityInput = document.getElementById(`quantity${index}`);
-        const totalPrice = document.getElementById(`total_price${index}`);
-        const price = quantityInput.dataset.price;
-        const finalPrice = totalPrice.innerText.replace('€', '');
-
-        if (Number(quantityInput.value) > 1) {
-            quantityInput.value = Number(quantityInput.value) - 1;
-            totalPrice.innerText = Number(finalPrice - price).toFixed(2) + " €";
-            cartTotal -= Number(price);
-            renderCartTotal(cartTotal);
-        }
-    });
-};
-
-// add each total products prices
-function calcCartTotal() {
-    const totalPrices = document.querySelectorAll(".total_price");
-    let totalText = '';
-    totalPrices.forEach(totalPrice => {
-        totalText = totalPrice.innerText.replace('€', '');
-        cartTotal += Number(totalText);
-    });
-    //  return cartTotal;
-};
-
-function removeProduct(btn, index) {
-    btn.addEventListener('click', () => {
-        let childToRemove = tableEl.querySelector(`.single_product${index}`);
-        const totalPrice = document.getElementById(`total_price${index}`);
-        const priceToSubtract = totalPrice.innerHTML.replace('€', '');
-        cartTotal -= Number(priceToSubtract);
-        tableEl.removeChild(childToRemove);
-        numOfItems--;
-        // calcCartTotal();
-        renderCartTotal(cartTotal);
-        renderNumberOfItems(numOfItems);
-    });
-};
-
-
-
-function applyPromoCode(promoCode) {
-    const promoMessage = document.getElementById('promoCodeMsg');
-    promoMessage.innerText = '';
-
-    if (promoCode == '') {
-        promoMessage.innerText = '';
-    } else if (!promoCodes.includes(promoCode)) {
-        promoMessage.innerText = 'Invalid Promo Code :(';
-        promoMessage.style.color = '#fa7272';
-    } else {
-        promoCode == 'megaultramaxipromo50' && (cartFullPrice -= cartFullPrice / 100 * 50);
-        promoCode == 'kebaboverdose80' && (cartFullPrice -= cartFullPrice / 100 * 80);
-        promoCode == 'fatgang25' && (cartFullPrice -= cartFullPrice / 100 * 25);
-        promoCode == 'eat-eat-repeat10' && (cartFullPrice -= cartFullPrice / 100 * 10);
-        promoMessage.innerText = `${promoCode.slice(-2)}% Promo Applied :)`
-        promoMessage.style.color = '#58c46a';
-    };
-};
+submitForm(cartFullPrice, promoCodes, totalCost); 
